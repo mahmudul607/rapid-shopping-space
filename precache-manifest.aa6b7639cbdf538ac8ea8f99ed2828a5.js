0e8aaf859a77d7d7d3b610ecf7d7b5dff944c0ce
mahmudul607 <111509406+mahmudul607@users.noreply.github.com>
2023-01-11 16:06:00 +0600

    Updates

---
+++ precache-manifest.aa6b7639cbdf538ac8ea8f99ed2828a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0690bd3c08700960ac0d",
+    "url": "/rapid-shopping-space/static/css/main.c6ac1cfe.chunk.css"
+  },
+  {
+    "revision": "0690bd3c08700960ac0d",
+    "url": "/rapid-shopping-space/static/js/main.0690bd3c.chunk.js"
+  },
+  {
+    "revision": "f09d6da400d0c14a7f1d",
+    "url": "/rapid-shopping-space/static/js/1.f09d6da4.chunk.js"
+  },
+  {
+    "revision": "fb19bcb9ba6f9e54c4a9",
+    "url": "/rapid-shopping-space/static/js/2.fb19bcb9.chunk.js"
+  },
+  {
+    "revision": "df979a79cdbc0f11f002",
+    "url": "/rapid-shopping-space/static/js/runtime~main.df979a79.js"
+  },
+  {
+    "revision": "a10ecfc614b0f2301dde0c682e4b49e2",
+    "url": "/rapid-shopping-space/static/media/favi.a10ecfc6.png"
+  },
+  {
+    "revision": "e201e423efceb10de00d13538e24b935",
+    "url": "/rapid-shopping-space/index.html"
+  }
+];
