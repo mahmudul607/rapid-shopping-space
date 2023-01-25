a50c283dfa9450746b981a1304d71488000d35a1
mahmudul607 <111509406+mahmudul607@users.noreply.github.com>
2023-01-25 20:09:15 +0600

    shop.js setProducts commentOut

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -8,7 +8,7 @@ import Product from '../Product/Product';
 import './Shop.css';
 const Shop = () => {
     const first10 = fakeData.slice(0, 20);
-    const [products, setProducts] = useState(first10);
+    // const [products, setProducts] = useState(first10);
     const [cart, setCart] = useState([]);
     useEffect(() =>{
         const saveCart = getDatabaseCart();
